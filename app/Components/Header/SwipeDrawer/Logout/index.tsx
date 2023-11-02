16b3d00d416dba1559ce0e058b48c1c3ab978191
ProgrammerFront077 <alirezakh0090@gmail.com>
2023-11-02 15:55:28 +0330

    (Update):<-Ui SwipeDrawer->

--- app/Components/Header/SwipeDrawer/Logout/index.tsx
+++ app/Components/Header/SwipeDrawer/Logout/index.tsx
@@ -15,7 +15,7 @@ const Logout = () => {
       <li className="w-full">
         <button
           onClick={handlerLogOut}
-          className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
+          className="flex items-center p-2 w-full text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
         >
           <Icons.MdLogin />
           <span className="ml-3">signout</span>
