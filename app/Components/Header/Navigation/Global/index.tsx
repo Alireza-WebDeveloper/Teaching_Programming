0b4a95e0a0545424cda3b2164b1168903ec3d0e3
ProgrammerFront077 <alirezakh0090@gmail.com>
2023-11-02 16:14:50 +0330

    (Update):<-Link Address Header->

--- app/Components/Header/Navigation/Global/index.tsx
+++ app/Components/Header/Navigation/Global/index.tsx
@@ -5,7 +5,7 @@ import * as Icons2 from 'react-icons/si';
 const GlobalLinks = () => {
   return (
     <>
-      <Link href="/home">
+      <Link href="/">
         <button className="px-5 py-2.5 flex gap-2 rounded-lg items-center bg-gray-50 dark:bg-gray-950 text-lg   relative capitalize hover:bg-black hover:text-white dark:hover:bg-white dark:hover:text-black">
           <span>home</span>
           <span>
